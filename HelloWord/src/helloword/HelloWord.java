3a774509408fcee490adec6b6c7a7a27f3a7381a
Aziz <Aziz@www.007guard.com>
2014-02-18 15:21:47 +0100

    GITHUB for the win.

---
+++ HelloWord/src/helloword/HelloWord.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+/**
+ *
+ * @author Aziz
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        System.out.println("Al salem Aleikom ");
+    
+    }
+}
